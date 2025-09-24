b7e09aacd325b7c90316b57473c04fc78870c397
Alexander Madsen <Alexandersm194@gmail.com>
2025-09-24 21:17:26 +0200

    Crown in dictionary and def for finalscore

--- MiniProject/AlexKernelTest.py
+++ MiniProject/AlexKernelTest.py
@@ -1,3 +1,5 @@
+import random
+
 import cv2
 import os
 import numpy as np
@@ -20,26 +22,28 @@ def input_image_folder():
         print("This is not a funtional path!")
         input_image_folder()
 
+
 def calculate_hue_hist(image):
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     hue_channel = hsv_image[:, :, 0]
     return cv2.calcHist([hue_channel], [0], None, [255], [0, 255])
 
-#Assigning values to each type op brick on the board found, so forest = 0, Stone = 1 ect.
-areaBrickDict = {"forestProb.jpg" : 0,
-                  "GreyProb.jpg" : 1,
-                  "MineProb.jpg" : 2,
-                  "planeProb.jpg" : 3,
-                  "WaterProb.jpg" : 4,
-                  "YellowProb.jpg" : 5,
-                  }
 
-brickDict = {"BrickType" : np.uint8(0),
-             "Crowns" : np.uint8(0),
-             "checked" : False}
+areaBrickDict = {"forestProb.jpg": 0,
+                  "GreyProb.jpg": 1,
+                  "MineProb.jpg": 2,
+                  "planeProb.jpg": 3,
+                  "WaterProb.jpg": 4,
+                  "YellowProb.jpg": 5}
 
-propertyDict = {"BrickType" : "",
-                "Count" : np.uint8(0)}
+brickDict = {"BrickType": np.uint8(0),
+             "Crowns": np.uint8(0),
+             "checked": False,
+             "ImageID": np.uint8(0)}
+
+propertyDict = {"BrickType": "",
+                "Count": np.uint8(0),
+                "Crowns": np.uint8(0)}
 
 image = cv2.imread("King Domino dataset//Cropped and perspective corrected boards//1.jpg")
 
@@ -77,7 +81,6 @@ for y in range(5):
             croppedImages.append(image[kernel_size_y * y:kernel_size_y * y + kernel_size_y, kernel_size_x * x:kernel_size_x * x + kernel_size_x])
 
 
-
 input_image_folder()
 
 for croppedImage in croppedImages:
@@ -104,29 +107,33 @@ for i in range(matrix.shape[0]):
     for j in range(matrix.shape[1]):
         var = areaBrickDict.get(brickTypes[croppedImgIndex])
         matrix[i, j]["BrickType"] = var
+        matrix[i, j]["ImageID"] = croppedImgIndex
         croppedImgIndex += 1
 
-print(matrix)
+
+def calculate_crowns_per_square(ImageID):
+    return random.randint(0, 2)
 
 
-def dfs(matrix, x, y, BrickType, in_count):
+def dfs(matrix, x, y, BrickType, in_count, crowns):
     # Check boundary conditions and color match
-    if (x < 0 or x >= 5 or y < 0 or
-            y >= 5 or matrix[y, x]["checked"] == True or matrix[y, x]["BrickType"] != BrickType):
-        return in_count
+    if (x < 0 or x >= 5 or y < 0 or y >= 5 or matrix[y, x]["checked"] == True
+            or matrix[y, x]["BrickType"] != BrickType):
+        return in_count, crowns
 
     matrix[y, x]["checked"] = True
 
     in_count = in_count + 1
 
+    crowns = crowns + calculate_crowns_per_square(matrix[y, x]["ImageID"])
+
     # Visit all adjacent pixels
-    in_count = dfs(matrix, x + 1, y, BrickType, in_count)
-    in_count = dfs(matrix, x - 1, y, BrickType, in_count)
-    in_count = dfs(matrix, x, y + 1, BrickType, in_count)
-    in_count = dfs(matrix, x, y - 1, BrickType, in_count)
+    in_count, crowns = dfs(matrix, x + 1, y, BrickType, in_count, crowns)
+    in_count, crowns = dfs(matrix, x - 1, y, BrickType, in_count, crowns)
+    in_count, crowns = dfs(matrix, x, y + 1, BrickType, in_count, crowns)
+    in_count, crowns = dfs(matrix, x, y - 1, BrickType, in_count, crowns)
 
-
-    return in_count
+    return in_count, crowns
 
 
 properties = []
@@ -135,17 +142,27 @@ for y in range(matrix.shape[0]):
     for x in range(matrix.shape[1]):
         if not matrix[y, x]["checked"]:
             brickType = matrix[y, x]["BrickType"]
-            count = dfs(matrix, x, y, brickType, 0)
+            count, crowns = dfs(matrix, x, y, brickType, 0, 0)
             matrix[y, x]["checked"] = True
 
             prop = propertyDict.copy()
             prop["BrickType"] = brickType
             prop["Count"] = count
+            prop["Crowns"] = crowns
             properties.append(prop)
 
 
-print(matrix)
+def calculate_final_score():
+    final_score = 0
+    for prop in properties:
+        final_score += prop["Count"] * prop["Crowns"]
+
+    return final_score
+
+
 print(properties)
 cv2.imshow("image", image)
+print(f"The final score is: {calculate_final_score()}")
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
